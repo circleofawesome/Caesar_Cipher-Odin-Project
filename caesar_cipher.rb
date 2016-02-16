2efe242f2ae4d2ffded64e9f5a48b69431f1fca5
Samiul Anwar <samiul.anwar@gmail.com>
2016-02-16 11:01:13 -0500

    finishing touches

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,11 +1,15 @@
-sentence="hello, world".split(//)
-alphabet=('a'..'z').to_a
+sentence="Hello, WORLD!!!".split('')
 cipher=[]
-alphabet_length=alphabet.length
-
-sentence.each do |i|
-    for x in 0..alphabet_length do
-    	
-    end
+sentence.each do |letter|
+	if (letter.ord>64&&letter.ord<86)||(letter.ord>96&&letter.ord<118)
+		num=letter.ord+5
+		cipher<<num.chr
+	elsif (letter.ord>85&&letter.ord<91)||(letter.ord>117&&letter.ord<123)
+			num=letter.ord-26+5
+			cipher<<num.chr
+	else
+		cipher<<letter
+	end
 end
 
+print cipher.join
