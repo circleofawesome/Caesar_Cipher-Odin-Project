40946bbe299449697c628adcf03d497ee88f6793
Samiul Anwar <samiul.anwar@gmail.com>
2016-02-16 00:16:33 -0500

    trying another technique

---
+++ caesar_cipher.rb
@@ -0,0 +1,11 @@
+sentence="hello, world".split(//)
+alphabet=('a'..'z').to_a
+cipher=[]
+alphabet_length=alphabet.length
+
+sentence.each do |i|
+    for x in 0..alphabet_length do
+    	
+    end
+end
+
